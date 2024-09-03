1bd20ea8e08b30b9d923263db08a851402a27dca
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2024-09-03 19:52:00 +0500

    Изменён host для раздачи статики swagger на unpkg.com

--- api/main.py
+++ api/main.py
@@ -2,6 +2,11 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from .database import Model, engine
 from .routers import pages
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
 
 
 @asynccontextmanager
@@ -10,5 +15,30 @@ async def lifespan(app: FastAPI):
         await conn.run_sync(Model.metadata.create_all)
     yield
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, docs_url=None, redoc_url=None)
 app.include_router(pages.router)
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="https://unpkg.com/redoc@next/bundles/redoc.standalone.js",
+    )
