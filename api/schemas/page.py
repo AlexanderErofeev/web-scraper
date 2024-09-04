01d8c8685ebd6e28e2e49c97b5fd3ed564fa6d69
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2024-09-04 23:02:29 +0500

    Рефакторинг примеров для swagger

--- api/schemas/page.py
+++ api/schemas/page.py
@@ -1,56 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class SPageBase(BaseModel):
-    title: str | None
-    url: str
+    title: str | None = Field(examples=['Экскурсии в Таиланде'])
+    url: str = Field(examples=['https://anextour.ru/excursions/thailand'])
 
-    model_config = {
-        "json_schema_extra": {
-            "example": {
-                "title": "Экскурсии в Таиланде",
-                "url": "https://anextour.ru/excursions/thailand",
-            }
-        }
-    }
+    model_config = ConfigDict(from_attributes=True)
 
 
 class SPageList(SPageBase):
-    id: int
-
-    model_config = {
-        "json_schema_extra": {
-            "example": {
-                **SPageBase.model_config.get("json_schema_extra").get("example"),
-                "id": 1873,
-            }
-        }
-    }
+    id: int = Field(examples=[4974])
 
 
 class SPageDetail(SPageList):
-    html: str | None
-
-    model_config = {
-        "json_schema_extra": {
-            "example": {
-                **SPageList.model_config.get("json_schema_extra").get("example"),
-                "html": "<!DOCTYPE html><html> ... </html>",
-            }
-        }
-    }
+    html: str | None = Field(examples=["<!DOCTYPE html><html> ... </html>"])
 
 
 class SPageAdd(SPageBase):
-    html: str | None
-
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    **SPageBase.model_config.get("json_schema_extra").get("example"),
-                    "html": "<!DOCTYPE html><html> ... </html>",
-                }
-            ]
-        }
-    }
+    html: str | None = Field(examples=["<!DOCTYPE html><html> ... </html>"])
