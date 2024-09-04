6aff2089484be18ba89f132994c3a00f42a5d8f6
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2024-09-04 22:15:06 +0500

    Исправление схемы API для корректного отображения примеров в swagger

--- api/schemas/page.py
+++ api/schemas/page.py
@@ -1,19 +1,56 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel
 
 
-class SPageList(BaseModel):
-    id: int
+class SPageBase(BaseModel):
     title: str | None
     url: str
 
-    model_config = ConfigDict(from_attributes=True)
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "Экскурсии в Таиланде",
+                "url": "https://anextour.ru/excursions/thailand",
+            }
+        }
+    }
+
+
+class SPageList(SPageBase):
+    id: int
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                **SPageBase.model_config.get("json_schema_extra").get("example"),
+                "id": 1873,
+            }
+        }
+    }
 
 
 class SPageDetail(SPageList):
     html: str | None
 
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                **SPageList.model_config.get("json_schema_extra").get("example"),
+                "html": "<!DOCTYPE html><html> ... </html>",
+            }
+        }
+    }
 
-class SPageAdd(BaseModel):
-    title: str | None
-    url: str
+
+class SPageAdd(SPageBase):
     html: str | None
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    **SPageBase.model_config.get("json_schema_extra").get("example"),
+                    "html": "<!DOCTYPE html><html> ... </html>",
+                }
+            ]
+        }
+    }
