3f9fdb8de37baaea2de59e1ebf22e95f9ade7a59
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2024-09-04 23:05:32 +0500

    Добавлены заголовки для полей схем

--- api/schemas/page.py
+++ api/schemas/page.py
@@ -2,19 +2,19 @@ from pydantic import BaseModel, ConfigDict, Field
 
 
 class SPageBase(BaseModel):
-    title: str | None = Field(examples=['Экскурсии в Таиланде'])
-    url: str = Field(examples=['https://anextour.ru/excursions/thailand'])
+    title: str | None = Field(title='Заголовок страницы', examples=['Экскурсии в Таиланде'])
+    url: str = Field(title='Адрес страницы', examples=['https://anextour.ru/excursions/thailand'])
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class SPageList(SPageBase):
-    id: int = Field(examples=[4974])
+    id: int = Field(title='ID страницы', examples=[4974])
 
 
 class SPageDetail(SPageList):
-    html: str | None = Field(examples=["<!DOCTYPE html><html> ... </html>"])
+    html: str | None = Field(title='Полный HTML страницы', examples=["<!DOCTYPE html><html> ... </html>"])
 
 
 class SPageAdd(SPageBase):
-    html: str | None = Field(examples=["<!DOCTYPE html><html> ... </html>"])
+    html: str | None = Field(title='Полный HTML страницы', examples=["<!DOCTYPE html><html> ... </html>"])
