a8409513233a9a11296ddcaf170a099ce7ad352c
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2024-09-04 14:53:48 +0500

    Реализован полнотекстовый поиск для pages/ по параметру q

--- api/repositories/pages.py
+++ api/repositories/pages.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select, func, text
 from ..models.page import Page
 from ..schemas.page import SPageList, SPageDetail, SPageAdd
 from ..database import SessionLocal
@@ -16,9 +16,25 @@ class PageRepository:
             return new_task.id
 
     @classmethod
-    async def get_pages(cls) -> list[SPageList]:
+    async def get_pages(cls, q: str = None) -> list[SPageList]:
         async with SessionLocal() as session:
-            query = select(Page)
+            await session.execute(text('CREATE EXTENSION IF NOT EXISTS pg_trgm'))
+
+            if q is None:
+                query = select(Page).limit(100)
+            else:
+                columns = func.coalesce(Page.title, '').concat(func.coalesce(Page.url, ''))
+                columns = columns.self_group()
+
+                query = select(
+                    Page,
+                ).where(
+                    func.similarity(columns, q) > 0.05
+                    # Page.title.bool_op('%')(q),
+                ).order_by(
+                    func.similarity(columns, q).desc(),
+                ).limit(100)
+
             result = await session.execute(query)
             task_models = result.scalars().all()
             tasks = [SPageList.model_validate(task_model) for task_model in task_models]
@@ -29,7 +45,6 @@ class PageRepository:
         async with SessionLocal() as session:
             query = select(Page).where(id == Page.id)
             result = await session.execute(query)
-            a = Page()
             task_model = result.scalars().one_or_none()
             if task_model is not None:
                 task = SPageDetail.model_validate(task_model)
--- api/routers/pages.py
+++ api/routers/pages.py
@@ -9,8 +9,8 @@ router = APIRouter(prefix="/pages")
 
 
 @router.get("/", response_model=list[SPageList])
-async def get_tasks():
-    pages = await PageRepository.get_pages()
+async def get_tasks(q: str = None):
+    pages = await PageRepository.get_pages(q)
     return pages
 
 
