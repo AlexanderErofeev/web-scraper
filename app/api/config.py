8decaefe49521a74c06fa16d38b0eb75037a9874
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2024-09-05 17:34:27 +0500

    Добавлено логирование для WEB API

--- app/api/config.py
+++ app/api/config.py
@@ -23,3 +23,49 @@ class ScraperSettings(BaseSettings):
         env_prefix = "SCRAPER_"
         env_file = ".env"
         extra = "ignore"
+
+
+LOG_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        'simple': {
+            'format': '[{asctime}] {levelname} {message}',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+            'style': '{',
+        },
+    },
+    "handlers":
+    {
+        "scraper_file":
+        {
+            "formatter": "simple",
+            "class": 'logging.FileHandler',
+            "filename": 'app/log/scraper.log'
+        },
+        "uvicorn_default_file":
+        {
+            "formatter": "simple",
+            "class": 'logging.FileHandler',
+            "filename": 'app/log/uvicorn_default.log'
+        },
+        "uvicorn_access_file":
+        {
+            "formatter": "simple",
+            "class": 'logging.FileHandler',
+            "filename": 'app/log/uvicorn_access.log'
+        },
+        "scraper":
+        {
+            "formatter": "simple",
+            "class": 'logging.StreamHandler',
+            # "level": "INFO",
+        },
+    },
+    "loggers":
+    {
+        "scraper": {"handlers": ["scraper", "scraper_file"], "level": "DEBUG", "propagate": False},
+        "uvicorn": {"handlers": ["uvicorn_default_file"], "level": "INFO", "propagate": False},
+        "uvicorn.access": {"handlers": ["uvicorn_access_file"], "level": "INFO", "propagate": False},
+    }
+}
--- app/api/main.py
+++ app/api/main.py
@@ -1,3 +1,5 @@
+import logging.config
+from .config import LOG_CONFIG
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from .database import Model, engine
@@ -15,6 +17,7 @@ async def lifespan(app: FastAPI):
         await conn.run_sync(Model.metadata.create_all)
     yield
 
+logging.config.dictConfig(LOG_CONFIG)
 app = FastAPI(lifespan=lifespan, docs_url=None, redoc_url=None)
 app.include_router(pages.router)
 
